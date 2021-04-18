fe1be715a9f1eae4fd369496e2d404b6bbd907e5
Robin Boers <robindev2019@outlook.com>
2021-04-18 17:52:42 +0200

    Updated fish, fonts, openbox and tint2 configs
    I updated my fish config. It now shows colorscript on startup. I also updated my openbox to look better when using it with plasma apps. I also added some keybindings and forced all emojis to use the Twitter Color Emoji font (twemoji)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,6 @@
+# Get same aliases as in bash_aliases
 source ~/.bash_aliases
+
+# Colorscript on startup yay
+colorscript -r
+
--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,3 +1,7 @@
-plank &
+export QT_QPA_PLATFORMTHEME=qt5ct
+
+xcape -e 'Super_L=Alt_L|F1'
+
 nitrogen --restore &
 xcompmgr &
+tint2 &

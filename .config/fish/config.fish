adae4e67c3daf153684e6ef30ea9f1a1d241e491
Robin Boers <robindev2019@outlook.com>
2021-04-28 20:04:13 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,12 @@ export QT_QPA_PLATFORMTHEME=qt5ct
 export PATH="$PATH:$HOME/.local/bin/"
 export EDITOR=nvim
 
+# Mouse stuff (using Arch now btw.)
+xinput set-prop "Synaptics TM3336-001" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "Synaptics TM3336-001" "libinput Tapping Enabled" 1
+
 # Set colorscheme
-theme.sh frontend-delight
+# theme.sh frontend-delight
 
 # Colorscript on startup yay
 colorscript -r

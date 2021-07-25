d871c21e523ed0378c0cf597e02c697300138167
Robin Boers <robindev2019@outlook.com>
2021-07-25 18:53:54 +0200

    Updated dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,7 @@ source ~/.bash_aliases
 export QT_QPA_PLATFORMTHEME=qt5ct
 export PATH="$PATH:$HOME/.local/bin/"
 export EDITOR=nvim
-
-# Mouse stuff (using Arch now btw.)
-xinput set-prop "Synaptics TM3336-001" "libinput Natural Scrolling Enabled" 1
-xinput set-prop "Synaptics TM3336-001" "libinput Tapping Enabled" 1
+export TERM=xterm-256color
 
 #set -l RANDOM_ASCII (random choice turtle milk stegosaurus eyes)
 
--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,3 @@
-# Defined in /home/robin/.config/fish/config.fish @ line 31
 function fish_greeting
 
 end
